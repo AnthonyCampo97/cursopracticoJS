63b231ba96732c8cb6618d11060d1ddd865c3e09
AnthonyCampo97_Pt <anthonycampo97@outlook.com>
2022-09-26 22:45:19 -0500

    se edita fragmentos del menu, se agregan script para desplegar los menus en mobile y desktop, se agregan lineas de codigo en los estilos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 //console.log('Hello, World')
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuIconMobile = document.querySelector(".menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener('click', toggleDesktopmenu);
+menuEmail.addEventListener("click", toggleDesktopmenu);
+menuIconMobile.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCaraside);
 
-function toggleDesktopmenu(){
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktopmenu() {
+  const isCarsideClosed = aside.classList.contains("inactive");
+  if (!isCarsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+  const isCarsideClosed = aside.classList.contains("inactive");
+  if (!isCarsideClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCaraside() {
+  const isDesktopmenu = desktopMenu.classList.contains("inactive");
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  if (!isMobileMenuClosed || !isDesktopmenu) {
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
